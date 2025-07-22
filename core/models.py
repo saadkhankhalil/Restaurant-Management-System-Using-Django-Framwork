63771d3be16382ba046ea4bce3b60070f0f6a2cb
saadkhankhalil <muhammadsaad03060509835@gmail.com>
2025-07-22 14:52:41 +0500

    add pending sales data table

--- core/models.py
+++ core/models.py
@@ -47,3 +47,23 @@ class Expense(models.Model):
 
     def __str__(self):
         return self.title
+    
+class PendingSale(models.Model):
+    customer_name = models.CharField(max_length=100, blank=True, null=True)
+    phone_no = models.CharField(max_length=20, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    tax = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    service_charges = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    payment_method = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+
+class PendingSaleDetail(models.Model):
+    pending_sale = models.ForeignKey(PendingSale, on_delete=models.CASCADE, related_name='details')
+    sr_no = models.IntegerField()
+    p_name = models.CharField(max_length=100)
+    p_qty = models.IntegerField()
+    p_price = models.DecimalField(max_digits=10, decimal_places=2)
+    p_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date_time = models.DateTimeField(auto_now_add=True)
+
